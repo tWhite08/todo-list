4bd60bbc9a858fde2e90b253e03a5544f645b224
Taylor White <taylorwhite1003@gmail.com>
2020-01-21 20:40:32 -0500

    todo-list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+//import logo from './logo.svg';
+//import './App.css';
 
+/* //Don't know if I still need this?
 function App() {
   return (
     <div className="App">
@@ -22,5 +23,101 @@ function App() {
     </div>
   );
 }
+*/
 
-export default App;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "Taylor White", //use your name here
+      todoItems: [
+        {action: "Complete Lab 2", done: false},
+        {action: "Publish to Github", done: false},
+        {action: "Sumbit Assignment", done: false}
+      ]
+    };
+    
+  }
+
+  /* // Code for name-change button
+  changeStateData = () => {
+    this.setState({
+      userName:
+      this.state.userName === "Taylor White" ? "Dalton" : 
+      "Taylor White"
+    });
+  };
+  */
+
+  updateNewTextValue = event => {
+    this.setState({ newItemText: event.target.value });
+  };
+
+  createNewTodo = () => {
+    if (
+      !this.state.todoItems.find(item => item.action === this.state.newItemText)
+    ) {
+      this.setState({
+        todoItems: [
+          ...this.state.todoItems,
+          {
+            action: this.state.newItemText,
+            done: false
+          }
+        ],
+      });
+    }
+  };
+
+  toggleTodo = todo =>
+    this.setState({
+      todoItems: this.state.todoItems.map(item =>
+          item.action === todo.action ? { ... item, done: !item.done } : item
+        )
+    });
+
+    todoTableRows = () =>
+      this.state.todoItems.map(item =>(
+        <tr key={item.action}>
+            <td>{item.action}</td>
+            <td>
+              <input
+                type="checkbox"
+                checked={item.done}
+                onChange={ () => this.toggleTodo(item)}
+              />
+            </td>
+        </tr>
+      ));
+
+  render = () => {
+    return (
+      <div>
+        <h4 className="bg-primary text-white text-center p-2">
+          {this.state.userName}'s To Do List
+        </h4>
+        <div className="containerfluid">
+          <div classname="my-1">
+            <input
+              classname="form-control"
+              value={this.state.newItemText}
+              onChange={this.updateNewTextValue}
+            />
+        <button className="btn btn-primary m-2" onClick={this.createNewTodo}>
+          Add
+        </button>
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Done</th>
+            </tr>
+          </thead>
+          <tbody>{this.todoTableRows()}</tbody>
+        </table>
+      </div>
+     </div>
+    </div>
+    );
+  }
+}
